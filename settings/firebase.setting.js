9e3cf43f64627f85ecd741a226d325a2120d1f53
Theaceemperor <omoobajesuexcellent@gmail.com>
2023-08-17 11:55:07 +0100

    removed evn setting setup

---
+++ settings/firebase.setting.js
@@ -0,0 +1,21 @@
+
+import { initializeApp } from "firebase/app";
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore'
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBvn8-oF12E3_Zsh82zNk1TOosWwDGWaNI",
+  authDomain: "facepal-359fe.firebaseapp.com",
+  projectId: "facepal-359fe",
+  storageBucket: "facepal-359fe.appspot.com",
+  messagingSenderId: "104867686648",
+  appId: "1:104867686648:web:9bbe2d364b7102fa78b30d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const authentication = getAuth(app);
+const db = getFirestore(app)
+
+export { authentication,db }

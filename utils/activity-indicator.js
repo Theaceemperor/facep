0ac6b6a1e74b5935898bf3fd24f477f2cb43d148
Theaceemperor <omoobajesuexcellent@gmail.com>
2023-08-29 11:58:14 +0100

    added activity indicator for general use

---
+++ utils/activity-indicator.js
@@ -0,0 +1,13 @@
+
+
+import { CircularProgress } from "@mui/material";
+
+
+export default function ActivityIndicator() {
+
+    return (
+        <div className='h-screen w-full absolute top-0 left-0 z-50 flex justify-center items-center bg-violet-400/10'>
+            <CircularProgress color='secondary'/>
+        </div>
+    )
+}

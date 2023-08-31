6ed22319395adb4d5a176a993b6e59e93e5517a9
Theaceemperor <omoobajesuexcellent@gmail.com>
2023-08-31 13:16:31 +0100

    configured adapter

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,6 +1,9 @@
 
 import NextAuth from "next-auth/next";
 import GoogleProvider from "next-auth/providers/google";
+import { cert } from "firebase-admin/app";
+import { FirestoreAdapter } from "@auth/firebase-adapter"
+
 
 export const authOptions = {
     providers: [
@@ -15,7 +18,14 @@ export const authOptions = {
 
             return session;
         }
-    }
+    },
+    adapter: FirestoreAdapter({
+        credential: cert({
+          projectId: process.env.FIREBASE_PROJECT_ID,
+          clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+          privateKey: process.env.FIREBASE_PRIVATE_KEY ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n") : undefined,
+        }),
+      }),
 }
 
 export default NextAuth(authOptions);
